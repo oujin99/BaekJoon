28c26ce819d3790d670d2918e79df4c8198c2884
SeoYuJin <oujin4174@gmail.com>
2023-01-25 17:16:17 +0900

    baekjoon 11021

---
+++ src/반복문/A와B보기좋게출력_11021.java
@@ -0,0 +1,44 @@
+package 반복문;
+
+import java.util.Scanner;
+
+public class A와B보기좋게출력_11021 {
+
+	public static void main(String[] args) {
+		/*
+		 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+		<입력>
+		첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+		각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+		<출력>
+		각 테스트 케이스마다 "Case #x: "를 출력한 다음, A+B를 출력한다. 테스트 케이스 번호는 1부터 시작한다.
+
+		<예제 입력> 	<출력>
+		5	
+		1 1			Case #1: 2
+		2 3			Case #2: 5
+		3 4			Case #3: 7
+		9 8			Case #4: 17
+		5 2			Case #5: 7
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		int testCase = sc.nextInt();
+		int[] result = new int[testCase];
+		
+		for (int i = 1; i <= testCase; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			result[i-1] += a + b;
+		}
+		for (int i = 0; i < result.length; i++) {
+			
+		System.out.println("Case #" + (i+1) + ": " + result[i]);
+		}
+		
+	}
+
+}
