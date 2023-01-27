d82df91a270d102ac6f13c07f38745b2ffecb0af
SeoYuJin <oujin4174@gmail.com>
2023-01-27 13:43:10 +0900

    baekjoon 2438 using BufferedReader

--- src/반복문/별찍기_2438.java
+++ src/반복문/별찍기_2438.java
@@ -1,11 +1,14 @@
 package 반복문;
 
-
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class 별찍기_2438 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		/*
 		 <문제>
 		첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
@@ -25,17 +28,21 @@ public class 별찍기_2438 {
 		*/
 		
 		
-		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
 		String star = "*";
 		
 		for (int i = 1; i <= num; i++) {
 			for (int j = 0; j < i; j++) {
-				System.out.print(star);
+				bw.write(star);
 			}
-			System.out.println();
+			bw.write("\n");
 		}
-		
+		bw.flush();
+		bw.close();
+		br.close();
 		
 
 	}
